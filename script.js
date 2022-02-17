b3c696b0a4e43a0942bc8f7d5a8ffbaf9910ba20
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2022-02-16 23:36:14 -0500

    set a window refresh button

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-var dogPictureEL= document.querySelector('#dogPicture')
+var dogPictureEL = document.querySelector("#dogPicture");
+var buttonEl = document.querySelector("#button");
 
-
-async function renderImage () {
-
-   const response= await fetch('https://dog.ceo/api/breeds/image/random')
-   const data = await response.json()
-    dogPictureEL.innerHTML = `<img id="dogStyle" src="${data.message}"/>`
-    
+function reload() {
+  window.location.reload();
 }
+
+async function renderImage() {
+  const response = await fetch("https://dog.ceo/api/breeds/image/random");
+  const data = await response.json();
+  dogPictureEL.innerHTML = `<img id="dogStyle" src="${data.message}"/>`;
+}
+
+buttonEl.addEventListener("click", reload)
 
 renderImage();
