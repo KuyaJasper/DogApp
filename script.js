562257ce704362984e105a566f35275c546399c0
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2022-02-16 22:45:32 -0500

    set up html and started fetch call from dog api

---
+++ script.js
@@ -0,0 +1,6 @@
+var dogPictureEL= document.querySelector('#dogPicture')
+
+
+fetch('https://dog.ceo/api/breeds/image/random')
+.then(response => response.json())
+  .then(data => console.log(data));
