b09d787397a007858371b30545aaabd58985ae99
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2022-02-16 22:47:48 -0500

    set up github.com and started a readme file

---
+++ readme.md
@@ -0,0 +1,5 @@
+started this app for practice fetching calls and a simple vanilla javascript app with a dog api
+
+
+Dog API
+https://dog.ceo/dog-api/
--- script.js
+++ script.js
@@ -4,3 +4,4 @@ var dogPictureEL= document.querySelector('#dogPicture')
 fetch('https://dog.ceo/api/breeds/image/random')
 .then(response => response.json())
   .then(data => console.log(data));
+
